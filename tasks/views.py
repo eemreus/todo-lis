17801bf7b8b53a5090aba332cb66cbe9d16633cb
eemreus <m.emreulas2003@gmail.com>
2025-05-05 10:25:33 +0300

    Tüm dosyaları ekledim

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        Task.objects.create(title=title)
+        return redirect('task_list')
+    return render(request, 'tasks/add_task.html')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
